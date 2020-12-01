f46a9a8b3db911843b185e60e2ab34ac7b1d6952
Andrea Iglio <andrea.iglio@gmail.com>
2020-12-01 14:50:59 +0100

    Task 1
    Showing an alert with 5 random and different numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,4 +23,34 @@ $(function () {
   
   // Functions
 
+
+// ESERCIZIO
+// TASK 1 - Un alert espone 5 numeri casuali diversi.
+
+  var numberList = [];
+
+  var i=0;
+
+  while (numberList.length < 5){
+
+  var x = Math.floor(Math.random() * Math.floor(100));
+
+  if(numberList.indexOf(x) == -1){
+   numberList.push(x);
+  }
+  
+  i++;
+}
+
+  console.log(numberList);
+
+  alert(numberList);
+
+  
+// Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
+// Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
+
+
+
+
 });
