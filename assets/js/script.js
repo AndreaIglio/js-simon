2c9bcbf5dbe6ccb212d7e6e34b51aa5d29c0f1d6
Andrea Iglio <andrea.iglio@gmail.com>
2020-12-01 17:43:06 +0100

    Improved code

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,89 +15,104 @@ $(document).ready(function () {
 // Codice viene eseguito solo quando documento e' carico
 $(function () {
   //codice jQuery
-  
-  
+
   // Variabili con valore
 
   // Variabili senza valore
-  
+
   // Functions
+  
+  /** functions which loop through an array
+   * @param array
+   * @param e - the element we will loop through the array to check if is include or not
+   * 
+   */
 
+  // function inArray(array, e) {
+  //   var i = 0;
+  //   var result = false;
+  //   while (i < array.length && result == false) {
+  //     if (array[i] == element) {
+  //       result = true;
+  //     }
+  //     i++;
+  //   }
+  //   return result;
+  // }
 
-// ESERCIZIO
-// TASK 1 - Un alert espone 5 numeri casuali diversi.
+  // ESERCIZIO
+  // TASK 1 - Un alert espone 5 numeri casuali diversi.
 
   var numberList = [];
 
-  var i=0;
+  var i = 0;
 
-  while (numberList.length < 5){
+  while (numberList.length < 5) {
+    var x = Math.floor(Math.random() * Math.floor(100));
 
-  var x = Math.floor(Math.random() * Math.floor(100));
+    if (numberList.indexOf(x) == -1) {
+      numberList.push(x);
+    }
 
-  if(numberList.indexOf(x) == -1){
-   numberList.push(x);
+    i++;
   }
-  
-  i++;
-}
 
   console.log(numberList);
 
   alert(numberList);
 
-  
-// Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
+  // Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
 
-setTimeout(userNumberList, 1000);
+  setTimeout(userNumberList, 1000);
 
-var userList = [];
-var matchedNumbers = [];
+  var matchedNumbers = [];
 
-function userNumberList (){
-  for (var i = 0; userList.length < 5; i++) {
-    var userNumber = Number(prompt("Inserisci un numero da 1 a 100"));
-    if (userList.indexOf(userNumber) == -1) {
-      userList.push(userNumber);
+  function userNumberList() {
+
+    var userList = [];
+
+    while (userList.length < 5) {
+      var userNumber = Number(prompt("Inserisci un numero da 1 a 100"));
+      if (userNumber > 100 || userNumber < 0) {
+        var userNumber = Number(prompt("Inserisci un numero da 1 a 100"));
+      } else {
+        if (userList.includes(userNumber) == false) {
+          userList.push(userNumber);
+          console.log(userList);
+          for (var i = 0; i < numberList.length; i++) {
+            if (userNumber == numberList[i]) {
+              matchedNumbers.push(userNumber);
+            }
+          }
+        }
+      }
     }
 
-    // Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
+    var x = matchedNumbers.length;
+    var y = matchedNumbers;
 
-    for (var i = 0; i < numberList.length; i++) {
-      if (userNumber == numberList[i]) {
-        matchedNumbers.push(userNumber);
-      }
-      // else {
-      //   console.log('You dont have any matched numbers');
-      // }
+    console.log(x);
+    console.log(y);
+
+    switch (matchedNumbers.length) {
+      case 0:
+        console.log("You dont have any matched numbers");
+        break;
+      case 1:
+        console.log("You have one number that match and is " + y);
+        break;
+      case 2:
+      case 3:
+      case 4:
+      case 5:
+        console.log(
+          "You have " +
+            x +
+            " matched numbers" +
+            ", the correct numbers are " +
+            y
+        );
+        break;
     }
   }
-
-  var x = matchedNumbers.length;
-  var y = matchedNumbers;
-
-  console.log(x);
-  console.log(y);
-
- switch (matchedNumbers.length) {
-   case 0:
-     console.log("You dont have any matched numbers");
-     break;
-   case 1:
-     console.log("You have one number that match and is " + y);
-     break;
-   case 2:
-   case 3:
-   case 4:
-   case 5:
-     console.log(
-       "You have " + x + " matched numbers" + ", the correct numbers are " + y
-     );
-     break;
- }
-}
-
-
-
-
 });
